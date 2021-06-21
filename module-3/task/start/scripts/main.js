977fd43be8c4c07ed5c0bb7ab6412bc79cedb9ce
eoin-oshea <eoin_oshea@hotmail.co.uk>
2021-06-21 11:48:11 +0100

    Update main.js
    Add constant, variables, call data to console log, write to screen.

---
+++ module-3/task/start/scripts/main.js
@@ -0,0 +1,30 @@
+const productName = 'Baked Beans'; //Creates an unchanging piece of data, with type String
+let price = 1.50; //Creates a variable, with type Number
+let quantity = 3;
+
+let inStock = true; //Creates a variable, with type Boolean
+let discountAmount = 0.20;
+
+console.log(productName); //Calls up data, regardless of type
+console.log(price);
+console.log(inStock);
+
+//typeof example
+console.log(typeof productName);
+console.log(typeof price);
+console.log(typeof inStock);
+//Calls up type of data in console
+
+
+
+console.log("Hello World"); //This prints a message to the console.log
+document.write("Hello World"); //This prints a message to the screen
+// This is a single line comment
+/*
+This
+Is
+A
+Multiple
+Line
+Comment
+*/
