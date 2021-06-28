d54b7e87197d9d77b0f04660e180ac6328104183
eoin-oshea <eoin_oshea@hotmail.co.uk>
2021-06-28 12:56:22 +0100

    Update main.js
    Commented out instructions to ensure fewer clashes in coding. Added loop to go over array stored in shoppingBasket.js, call up information, add discounts to specific types, and work out how to easily specify certain data entries within the array. Added comment log to explain psuedo-code.

--- module-3/task/start/scripts/main.js
+++ module-3/task/start/scripts/main.js
@@ -252,7 +252,7 @@ let hotel = {
     }
 }
 
-console.log(hotel['checkAvailability']());
+//console.log(hotel['checkAvailability']());
 
 /*let nameOfProduct = product.productName;
 let discount = product['discountAmount'];*/
@@ -262,8 +262,116 @@ let discount = product['discountAmount'];*/
     console.log(i);
 }*/
 
-let numbers = [1,2,3,4,5];
+/*let numbers = [1,2,3,4,5];
 
 for (let counter=0; counter <= 4; counter++) {
     console.log(numbers[counter]);
+}*/
+
+//code to call up data from shoppingBasket.js
+//console.log(shoppingBasket[0].type);
+function totalPriceOfShopping( arr ) {
+    let totalPrice = 0;
+    for(let counter = 0; counter < arr.length; counter++ ) {
+
+        //for loop goes over each item
+
+        totalPrice = totalPrice + ( arr[counter].price * arr[counter].quantity );
+
+        // console.log( totalPrice );
+        // totalPrice = 0
+        // 0 = 0 + (1.20 * 1)
+        // 1.20 = 1.20 + (1.79 * 1)
+        // console.log( arr[counter].price * arr[counter].quantity );
+
+
+    }
+    //toFixed rounds the number to whatever number of decimals are defined in the brackets
+    return totalPrice.toFixed(2);
+
+    //console.log( arr[3].type ); //calls up the type of the 4th product on the array
 }
+
+//console.log( `Total Price: £ ${totalPriceOfShopping( shoppingBasket )}` );
+
+function discountToiletries( arr ){
+    let totalPrice = 0;
+
+    for(let counter = 0; counter < arr.length; counter++ ) {
+
+        if(arr[counter].type === 'toiletries') {
+            totalPrice = totalPrice + (( arr[counter].price / 2 ) * arr[counter].quantity);
+        } else {
+            totalPrice = totalPrice + ( arr[counter].price *arr[counter].quantity);
+        }
+
+        //  console.log(arr[counter].type);
+    }
+
+    return totalPrice.toFixed(2);
+
+}
+
+//console.log( `Total Price: £ ${discountToiletries(shoppingBasket)}` );
+//console.log(shoppingBasket.length);
+
+//function with more customisable components
+function discount( arr, discountNum, type ) {
+    let totalPrice = 0;
+
+    for(let counter = 0; counter < arr.length; counter++ ) {
+
+        if(arr[counter].type === type) {
+
+            totalPrice = totalPrice + (( arr[counter].price - discountNum ) * arr[counter].quantity);
+        } else {
+            totalPrice = totalPrice + ( arr[counter].price *arr[counter].quantity);
+        }
+
+    }
+
+    return totalPrice.toFixed(2);
+
+}
+
+//console.log( discount( shoppingBasket, 1.20, "dairy") );
+
+/*
+There is a shopping basket containing different items
+
+from this shopping basket, we need to know how many of each item
+exists in a particular type
+
+We need somewhere to store the total
+It will be empty to start so it will start with the value of 0,
+because it will change how I will use the keyword let
+
+How am I going to cycle over the information (shopping basket)?
+
+    for(START COUNTER, CONDITION, INCREMENT)
+        Select first item
+            check type
+
+    if(ITEMS.TYPE === TYPE YOU ARE LOOKING FOR){
+        totalAmount + ITEMS.QUANTITY
+    }
+
+    RETURN totalAmount
+*/
+
+
+function howMany( arr, type ) {
+
+    let totalAmount = 0;
+
+    for( let counter = 0; counter < arr.length; counter++ ) {
+    
+        if( arr[counter].type === type ) {
+            totalAmount = totalAmount + arr[counter].quantity;
+        }
+    }
+    return totalAmount;
+
+}
+
+console.log(howMany( shoppingBasket, 'dairy')); //total should be 9
